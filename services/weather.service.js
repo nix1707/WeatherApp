451df2a3d70136755388b0a72711b493af596e92
nix1707 <121435639+nix1707@users.noreply.github.com>
2023-08-29 13:00:18 +0300

    Initial Commit

--- services/weather.service.js
+++ services/weather.service.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import api from './api'
-import React from 'react';
 
 export const WeatherService = {
     
     async getForecast(cityName){
-        const response = await axios.get(`${api.base}forecast.json?q=${cityName}&days=3&key=${api.key}`)
+        const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?q=${cityName}&days=3&key=${api.key}`)
         return response.data;
     }
 }
